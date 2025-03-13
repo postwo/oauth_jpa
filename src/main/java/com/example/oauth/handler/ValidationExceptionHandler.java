1af5ca70320af919529e361da838aa8457db97ac
postwo <codo7717@naver.com>
2025-03-14 05:00:55 +0900

    validationhandler 구현

---
+++ src/main/java/com/example/oauth/handler/ValidationExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.oauth.handler;
+
+import com.example.oauth.dto.response.ResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ValidationExceptionHandler {
+    //에러 메시지 전송
+
+
+    //execptionhandler ==특정 예외가 발생했을 때 해당 메서드를 호출하도록 지정
+    //MethodArgumentNotValidException == 요청이 유효성 검증을 통과하지 못했을 때 발생
+    //HttpMessageNotReadableException == 요청 본문이 읽을 수 없는 형식일 때 발생
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class}) //중괄호 쳐서 배열로 전달
+    public ResponseEntity<ResponseDto> validationExceptionHandler(Exception exception){
+        return ResponseDto.validationFail();
+    }
+
+}
